9393a7e948afe082e0245cbc60f2c8cebe68a3d9
bexxin <109035845+bexxin@users.noreply.github.com>
2023-10-18 17:41:50 -0400

    added getters and setters

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -14,6 +14,38 @@ public abstract class Vehicle {
         this.year=year;
 
     }
+    //getter & setters
+
+    public String getMake() {
+        return make;
+    }
+    public String getModel(){
+        return model;
+    }
+    public String getColor(){
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
     //method
     public String toString(){
         return String.format("----Vehicle Information----\nMake:%s\nModel:%s\nColor:%s\nPrice:%4.2f\nYear:%d",make, model,color, price,year);

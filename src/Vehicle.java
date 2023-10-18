06a2caef205b050143e63b9618ab1cbc2eb264a9
bexxin <109035845+bexxin@users.noreply.github.com>
2023-10-18 17:33:34 -0400

    initial commit, abstract class with variables.

---
+++ src/Vehicle.java
@@ -0,0 +1,9 @@
+public abstract class Vehicle {
+    //Variables
+    private String make;
+    private String model;
+    private String color;
+    private double price;
+    private int year;
+
+}

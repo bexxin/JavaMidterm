57f464a3b0519890382aacd83cde5e06d32edaf8
bexxin <109035845+bexxin@users.noreply.github.com>
2023-10-18 18:51:50 -0400

    semi-finalfinal draft, going to add error handling

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,44 @@
 import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        //Get attribute information from user.
-        String type= JOptionPane.showInputDialog("Is this for an SUV or a Sedan(please enter SUV or SED)?:");
-        String make= JOptionPane.showInputDialog("Please enter make:");
-        String model= JOptionPane.showInputDialog("Please enter model:");
-        String color= JOptionPane.showInputDialog("Please enter color:");
-        int year = Integer.parseInt(JOptionPane.showInputDialog("Enter Year:"));
-        double price = Double.parseDouble(JOptionPane.showInputDialog("Enter price:"));
-        //Create vehicle object based on user input
-        Vehicle vehicle=null;
-
-        if(type.equalsIgnoreCase("SUV")){
-            vehicle = new SUV(make,model,color,price,year,type);
-        } else if (type.equalsIgnoreCase("SED")) {
-            vehicle = new Sedan(make,model,color,price,year,type);
-        }else{
-            JOptionPane.showMessageDialog(null,"Invalid vehicle type.");
+        //array list to store vehicles
+        ArrayList<Vehicle> vehicles = new ArrayList<>();
+        //loop to create multiple vehicles and store in the arraylist.
+        while(true) {
+            //Get attribute information from user.
+            String type = JOptionPane.showInputDialog("Is this for an SUV or a Sedan(please enter SUV or SED)?:");
+            String make = JOptionPane.showInputDialog("Please enter make:");
+            String model = JOptionPane.showInputDialog("Please enter model:");
+            String color = JOptionPane.showInputDialog("Please enter color:");
+            int year = Integer.parseInt(JOptionPane.showInputDialog("Enter Year:"));
+            double price = Double.parseDouble(JOptionPane.showInputDialog("Enter price:"));
+            //Create vehicle object based on user input
+            Vehicle vehicle = null;
+            //call appropriate constructor based on type input
+            if (type.equalsIgnoreCase("SUV")) {
+                vehicle = new SUV(make, model, color, price, year, type);
+            } else if (type.equalsIgnoreCase("SED")) {
+                vehicle = new Sedan(make, model, color, price, year, type);
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid vehicle type.");
+                continue;
+            }
+            //Add vehicles to a list
+            vehicles.add(vehicle);
+            //Create another vehicle?
+            int choice = JOptionPane.showConfirmDialog(null,"Do you want to create another vehicle?");
+            if(choice!=JOptionPane.YES_OPTION){
+                break;}
         }
+
         //Display the vehicle information.
-        assert vehicle != null;
-        String vehicleInfo = vehicle.toString();
-        JOptionPane.showMessageDialog(null,vehicleInfo);
-
-
+        for (Vehicle vehicle : vehicles) {
+            String vehicleInfo = vehicle.toString();
+            JOptionPane.showMessageDialog(null,vehicleInfo);
+        }
     }
 }

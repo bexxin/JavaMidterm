65ec9fbf86eaf9e69dc08300d38a863ca78b3c91
bexxin <109035845+bexxin@users.noreply.github.com>
2023-10-18 17:35:39 -0400

    added constructor

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -5,5 +5,16 @@ public abstract class Vehicle {
     private String color;
     private double price;
     private int year;
+    //Constructor
+    public Vehicle(String make, String model, String color,double price, int year){
+        this.make=make;
+        this.model=model;
+        this.color=color;
+        this.price=price;
+        this.year=year;
+
+    }
+    //method
+    public String
 
 }
